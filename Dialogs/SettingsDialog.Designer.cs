616e6961fe5ddfd8b9249e07623626bc1636bf68
HyperCreeck@Cadex <52355164+yeppiidev@users.noreply.github.com>
2021-09-15 15:17:11 +0530

    Update SettingsDialog.Designer.cs

--- Dialogs/SettingsDialog.Designer.cs
+++ Dialogs/SettingsDialog.Designer.cs
@@ -23,12 +23,87 @@
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent() {
-            this.components = new System.ComponentModel.Container();
+            this.titleLbl = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.applyBtn = new System.Windows.Forms.Button();
+            this.cancelBtn = new System.Windows.Forms.Button();
+            this.okBtn = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // titleLbl
+            // 
+            this.titleLbl.AutoSize = true;
+            this.titleLbl.Font = new System.Drawing.Font("Microsoft YaHei UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLbl.Location = new System.Drawing.Point(16, 17);
+            this.titleLbl.Name = "titleLbl";
+            this.titleLbl.Size = new System.Drawing.Size(155, 46);
+            this.titleLbl.TabIndex = 0;
+            this.titleLbl.Text = "Settings";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Control;
+            this.panel1.Controls.Add(this.okBtn);
+            this.panel1.Controls.Add(this.cancelBtn);
+            this.panel1.Controls.Add(this.applyBtn);
+            this.panel1.Location = new System.Drawing.Point(-2, 290);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(548, 43);
+            this.panel1.TabIndex = 1;
+            // 
+            // applyBtn
+            // 
+            this.applyBtn.Location = new System.Drawing.Point(461, 10);
+            this.applyBtn.Name = "applyBtn";
+            this.applyBtn.Size = new System.Drawing.Size(75, 23);
+            this.applyBtn.TabIndex = 0;
+            this.applyBtn.Text = "Apply";
+            this.applyBtn.UseVisualStyleBackColor = true;
+            // 
+            // cancelBtn
+            // 
+            this.cancelBtn.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.cancelBtn.Location = new System.Drawing.Point(380, 10);
+            this.cancelBtn.Name = "cancelBtn";
+            this.cancelBtn.Size = new System.Drawing.Size(75, 23);
+            this.cancelBtn.TabIndex = 1;
+            this.cancelBtn.Text = "Cancel";
+            this.cancelBtn.UseVisualStyleBackColor = true;
+            // 
+            // okBtn
+            // 
+            this.okBtn.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.okBtn.Location = new System.Drawing.Point(299, 10);
+            this.okBtn.Name = "okBtn";
+            this.okBtn.Size = new System.Drawing.Size(75, 23);
+            this.okBtn.TabIndex = 2;
+            this.okBtn.Text = "OK";
+            this.okBtn.UseVisualStyleBackColor = true;
+            // 
+            // SettingsDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "SettingsDialog";
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(546, 332);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.titleLbl);
+            this.Name = "SettingsDialog";
+            this.ShowIcon = false;
+            this.Text = "Preferences";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label titleLbl;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button cancelBtn;
+        private System.Windows.Forms.Button applyBtn;
+        private System.Windows.Forms.Button okBtn;
     }
 }
