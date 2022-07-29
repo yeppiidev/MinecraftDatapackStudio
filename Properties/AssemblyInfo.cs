4c6ea617a10d9786b7f2c8dc2b87dea054f80932
Y2K4 <29883287+y2k04@users.noreply.github.com>
2022-07-29 15:07:37 +1000

    - Increased revision number by 1 - Changed copyright year to 2022

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("YeppiiDev")]
 [assembly: AssemblyProduct("Minecraft Datapack Studio")]
-[assembly: AssemblyCopyright("Copyright © YeppiiDev 2021")]
+[assembly: AssemblyCopyright("Copyright © YeppiiDev 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.12")]
-[assembly: AssemblyFileVersion("1.0.0.12")]
+[assembly: AssemblyVersion("1.0.0.13")]
+[assembly: AssemblyFileVersion("1.0.0.13")]
